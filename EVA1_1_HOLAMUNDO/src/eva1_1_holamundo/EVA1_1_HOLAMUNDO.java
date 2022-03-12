3c57d23e84e5d2bbe3124a4e11f0e8a62dd9bfcd
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-03-12 02:32:32 -0700

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author Narahi Zavala
+ */
+public class EVA1_1_HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+    
+}
